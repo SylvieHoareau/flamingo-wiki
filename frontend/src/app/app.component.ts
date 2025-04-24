f48f728084c26435a7833a5753e27403fe1e3c4b
hoareausylvie <hoareauylvie@outlook.fr>
2025-04-24 21:56:03 +0400

    styles: amélioration du style de la navbar et du footer

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
 // Composants Angular Material UI
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
@@ -13,6 +14,7 @@ import { MatMenuModule } from '@angular/material/menu';
   imports: [
     RouterOutlet,
     NavbarComponent,
+    FooterComponent,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
