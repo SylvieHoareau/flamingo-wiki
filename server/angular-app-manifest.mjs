6fe4bbb676d5a9a4ca4c5487e19193c36127af87
hoareausylvie <hoareauylvie@outlook.fr>
2025-04-25 07:55:21 +0400

    Auto-generated commit

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -14,8 +14,8 @@ export default {
   ]
 },
   assets: {
-    'index.csr.html': {size: 27095, hash: '8e5c9b617a8080a18f1f1dcf3f383c91f68383f6da84e3e6499d93ba86d17953', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 20484, hash: '823379a9b589bcba127d2b6b977a765f7549cb6a6f54dd16754ed4cf78590dcd', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 27155, hash: '179d69cc11800f2b97c8c72c6acf11108b287fc38c1b6b765c65f8b82485150d', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 20545, hash: '6f7f40ca3dcde4dc749edd75e66511d4e5d7c4df1a7fa4a5e3f22e6f51167a80', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-XHNVJ6LU.css': {size: 18956, hash: 'ZgVC/tXFjNk', text: () => import('./assets-chunks/styles-XHNVJ6LU_css.mjs').then(m => m.default)}
   },
 };
